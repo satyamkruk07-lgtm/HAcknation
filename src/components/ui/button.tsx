33b02d7033d46ec1f469f137c2fbe218b71691db
Kumar Satyam <satyamkruk07@gmail.com>
2025-11-01 15:40:00 +0000

    touch krne se pahle iska border dikhe in 3d view (_for element <LinkComp

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -16,7 +16,7 @@ const buttonVariants = cva(
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
+        ghost: "border border-input shadow-sm hover:bg-accent hover:text-accent-foreground active:shadow-inner active:translate-y-px",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
