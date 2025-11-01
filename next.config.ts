08ae547d883f4f05b93f28f4da61e37b327b2d27
Kumar Satyam <satyamkruk07@gmail.com>
2025-11-01 18:29:22 +0000

    ab jo tum code improvement kiye ho google sheet ke liye?

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    // This allows the Next.js dev server to accept requests from the
+    // Firebase Studio preview URL.
+    allowedDevOrigins: ["*.cloudworkstations.dev"],
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
