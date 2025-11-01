914d3fc38b9db94118db2d15e9bbe90d16c0e4ae
Kumar Satyam <satyamkruk07@gmail.com>
2025-11-01 14:20:18 +0000

    jo circle a uska line colur ho visible ho (_for element <Primitive.span>

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -12,7 +12,7 @@ const Avatar = React.forwardRef<
   <AvatarPrimitive.Root
     ref={ref}
     className={cn(
-      "relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full",
+      "relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full border-2 border-primary/20",
       className
     )}
     {...props}
