017dfda382edfcb6ca869d535d934dd11973b5e0
Kumar Satyam <satyamkruk07@gmail.com>
2025-11-03 17:01:48 +0000

    Please update the image here to the newly chosen ones (_for element img_

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -82,7 +82,7 @@ const hackathonTips = [
 ]
 
 const conductors = [
-  { name: 'Kumar Satyam', role: 'Student', seed: 'conductor1', imageUrl: 'https://images.unsplash.com/photo-1552374196-c4e7cc224247?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfGFsbHx8fHx8fHx8fDE3NjIxODg1Nzh8&ixlib=rb-4.0.3&q=80&w=1080' },
+  { name: 'Kumar Satyam', role: 'Student', seed: 'conductor1', imageUrl: 'https://images.unsplash.com/photo-1490138139357-fc819d02e344?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxMnx8Ym95fGVufDB8fHx8MTc2MjE4OTI0N3ww&ixlib=rb-4.1.0&q=80&w=1080' },
   { name: 'Kalyani Kumari', role: 'Student', seed: 'conductor2', imageUrl: 'https://images.unsplash.com/photo-1552376432-27da8aaf5c78?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwyfHxnaXJsJTIwaGlkZGVufGVufDB8fHx8MTc2MjE4ODY4OHww&ixlib=rb-4.1.0&q=80&w=1080' },
   { name: 'Paramjeet Singh', role: 'Faculty', seed: 'conductor3' },
   { name: 'Akshat Sharma', role: 'Faculty', seed: 'conductor4' },
@@ -482,3 +482,5 @@ export default function DashboardPage() {
     </div>
   );
 }
+
+    
