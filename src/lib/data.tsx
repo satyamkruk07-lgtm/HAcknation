35b6ddbfdc3cd7ec8fec9138e6728f6ff809c2ec
Kumar Satyam <satyamkruk07@gmail.com>
2025-12-12 08:49:57 +0000

    yaha pr Assistant Professor likh do (_for element p_)

--- src/lib/data.tsx
+++ src/lib/data.tsx
@@ -391,7 +391,7 @@ export const conductors: Conductor[] = [
     {
         id: "6",
         name: "Mr. Kshitij Jain",
-        role: "Faculty",
+        role: "Assistant Professor",
         imageUrl: "https://images.unsplash.com/photo-1765519817953-8f4206a73b70?q=80&w=738&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         email: "anshul.namdev@shivalikcollege.edu.in",
         linkedin: "https://www.linkedin.com/in/anshul-namdev",
@@ -446,6 +446,7 @@ export const conductors: Conductor[] = [
 
 
     
+
 
 
 
