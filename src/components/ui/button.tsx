da931d779c272f2be979b8d160225e54254f316b
Kumar Satyam <satyamkruk07@gmail.com>
2025-12-12 18:10:10 +0000

    ye website itna slow kyu hai?

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -16,7 +16,7 @@ const buttonVariants = cva(
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "border border-input shadow-sm hover:bg-accent hover:text-accent-foreground active:shadow-inner active:translate-y-px",
+        ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
