fca0a46a94f5b6383f187af89fd81ee1e17cddd7
Kumar Satyam <satyamkruk07@gmail.com>
2025-12-12 08:39:48 +0000

    akshat sharma ka name change krke Mr. Ajay Kumar Vermaye likh do

--- src/lib/data.tsx
+++ src/lib/data.tsx
@@ -379,7 +379,7 @@ export const conductors: Conductor[] = [
     },
     {
         id: "5",
-        name: "Mr. Akshat Sharma",
+        name: "Mr. Ajay Kumar Verma",
         role: "Faculty",
         imageUrl: "https://images.unsplash.com/photo-1765528447514-b85fbdd0e43d?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         email: "rakhi@shivalikcollege.eduin",
@@ -446,6 +446,7 @@ export const conductors: Conductor[] = [
 
 
     
+
 
 
 
