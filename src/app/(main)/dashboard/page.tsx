115782ab7200cab6c84a16b342be3e0a83096aa7
Kumar Satyam <satyamkruk07@gmail.com>
2025-12-12 07:36:18 +0000

    ye circle remove kro (_for element div_)

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -246,9 +246,6 @@ export default function DashboardPage() {
                 </Button>
             </div>
             <div className="flex justify-center items-center gap-8">
-              <div className="h-20 w-20 rounded-full bg-destructive/10 border-4 border-destructive/20 flex items-center justify-center">
-                <Image src="https://image3.mouthshut.com/images/imagesp/925718552s.png" alt="Shivalik College Logo" width={60} height={60} />
-              </div>
               <div className="text-center">
                 <h1 className="text-5xl font-bold font-headline tracking-tighter text-destructive">
                   Shivalik
